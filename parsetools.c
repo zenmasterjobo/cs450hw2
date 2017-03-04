3ae2304a808d7f6edb09df45617553d7733c901a
Jordan Bergero <bergero@seawolf.sonoma.edu>
2017-03-04 13:12:05 -0800

    catching up to master

--- parsetools.c
+++ parsetools.c
@@ -21,34 +21,7 @@ int split_cmd_line(char* line, char** list_to_populate) {
    while(list_to_populate[i] != NULL && i < MAX_LINE_WORDS - 1)  { // MAX_LINE_WORDS 100
        list_to_populate[++i] = strtok_r(NULL, delimiters, &saveptr);
    };
-
-   run_command(list_to_populate);
+   
    return i;
    
 }
-void run_command(char** list_to_populate){
-  
-  int child = fork();
-  int hold;
-  printf("child: %d\n",child);
-  printf("current id %d\n", getpid());
-  
-  if(child == 0){
-
-    
-    execvp(list_to_populate,list_to_populate);
-    
-    printf("Test before Exec\n");
-    execvp(cmd, args);
-    printf ("Test after Exec\n");
-  }
-  else{
-    hold = wait(NULL);
-  }
-  
-  printf ("Test outside Exec\n");
-  printf ("Wait: %d\n", hold);
-  
-}
-
-ps -u jbergero
