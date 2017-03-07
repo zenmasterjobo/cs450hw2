3f90b56fd090b2af4f498d33b10c1bfeadb276ea
Eugene <riveraeu@seawolf.sonoma.edu>
2017-03-07 09:10:29 -0800

    single pipe and < or > redirect working

--- main.c
+++ main.c
@@ -7,59 +7,95 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
 void pipe_dream();
 void syserror(const char *);
+void redirectInputOutput(char* left[], char* right[], char* symbol[]);
 
-int main()
-{
-	// Buffer for reading one line of input
-	char line[MAX_LINE_CHARS];
-	char* line_words[MAX_LINE_WORDS + 1];
-	int pipes_num = 0;
-	// Loop until user hits Ctrl-D (end of input)
-	// or some other input error occurs
-	printf("\n $: ");
-	while( fgets(line, MAX_LINE_CHARS, stdin) )
-	{
-		int num_words = split_cmd_line(line, line_words);
-		
-		int child = fork();
-			
-		if(child == 0)
-		{
-			for(int i = 0; i < num_words; i++)
-			{
-				printf("line_words[%d] = %s\n", i, line_words[i]);
-				//if(line_words[i] == '|')
-				if(strcmp(line_words[i], "|") == 0)
-				{
-					pipes_num++;
-					printf(" Pipes: %d\n", pipes_num);
-				}
-			}
-			
-			if(pipes_num == 0)
-			{
-				execvp(*line_words, line_words);
-			}
-			
-			else
-				printf("Found a pipe. \n");
-		}
-		
-		wait(NULL);
-		printf("\n $: ");
-	}
-	
-	return 0;
+int main(){
+  // Buffer for reading one line of input
+  char line[MAX_LINE_CHARS];
+  char* line_words[MAX_LINE_WORDS + 1];
+  char* left[MAX_LINE_WORDS +1];
+  char* right[MAX_LINE_WORDS +1];
+  char* symbol[1];
+  int pipes_num = 0;
+  // Loop until user hits Ctrl-D (end of input)
+  // or some other input error occurs
+  printf("\n $: ");
+  while( fgets(line, MAX_LINE_CHARS, stdin)){
+    int num_words = split_cmd_line(line, line_words);
+    /*Just for demonstration purposes
+    for (int i=0; i < num_words; i++)
+        printf("%s\n", line_words[i]);
+    printf("numwords: %d\n", num_words);
+    */
+    int child = fork();
+    if(child == 0){
+      int redirect = 0;
+      for(int i = 0; i < num_words; i++){
+        if(strcmp(line_words[i], ">") == 0 || strcmp(line_words[i], "<") == 0 || strcmp(line_words[i], "|") == 0){
+          redirect = 1;
+			    symbol[0] = line_words[i];
+          for(int j = 0; j <= i; j++){
+              if(j == i)
+                  left[j] = NULL;
+              else
+                  left[j] = line_words[j];
+              printf("left[%d]: %s\n", j, left[j]);
+          }
+          i++;
+          for(int j = 0; j+i <= num_words; j++){
+              if(j+i == num_words)
+                  right[j] = NULL;
+              else
+                  right[j] = line_words[j+i];
+              printf("right[%d]: %s\n", j, right[j]);
+          }
+          i--;
+					if (*symbol[0] == '>')
+          	redirectInputOutput(left, right, symbol);
+					else if (*symbol[0] == '<')
+						redirectInputOutput(left, right, symbol);
+					else
+						pipe_dream(left, right);
+        }
+      }
+      if (!redirect)
+      // no redirection needed
+        execvp(*line_words, line_words);
+    }
+    wait(NULL);
+    printf("\n $: ");
+  }
+  return 0;
 }
 
-void pipe_dream(){
+void redirectInputOutput(char* left[], char* right[], char* symbol[]){
+    // Input for command is read from a file when < symbol is passed
+    // and output for command is sent to a file when > symbol is passed
+    pid_t pid;
+    if (*symbol[0] == '>'){
+      int fd = open(right[0], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+      close(1); //close stdout
+      dup(fd); // duplicate fd into stdout, reference line above
+      execvp(*left, left); //execute the command
+    }
+    else{
+      int fd = open(left[0], O_RDONLY, 0777);
+      printf("%s\n", left[0]);
+      close(0);
+      dup(fd);
+      execvp(*right, right);
+    }
+    wait(NULL); //parent will wait for child to finish
+}
 
+void pipe_dream(char* left[], char* right[]){
 	int pfd[2];
 	pid_t pid;
-  
+	printf("left: %s\n", left[0]);
 	if ( pipe (pfd) == -1 )
 		syserror( "Could not create a pipe" );
 	switch ( pid = fork() ) {
@@ -71,7 +107,7 @@ void pipe_dream(){
 		dup(pfd[0]);
 		if ( close (pfd[0]) == -1 || close (pfd[1]) == -1 )
 			syserror( "Could not close pfds from first child" );
-		execlp("wc", "wc", NULL);
+		execvp(*right, right);
 		syserror( "Could not exec wc");
 	}
 	fprintf(stderr, "The first child's pid is: %d\n", pid);
@@ -84,7 +120,7 @@ void pipe_dream(){
 		dup(pfd[1]);
 		if ( close (pfd[0]) == -1 || close (pfd[1]) == -1 )
 			syserror( "Could not close pfds from second child" );
-		execlp("who", "who", NULL);
+		execvp(*left, left);
 		syserror( "Could not exec who" );
 	}
 	fprintf(stderr, "The second child's pid is: %d\n", pid);
@@ -93,14 +129,12 @@ void pipe_dream(){
 	if (close(pfd[1]) == -1)
 		syserror( "Parent could not close stdout" );
 	while ( wait(NULL) != -1) ;
+	printf("done with pipe_dream\n");
 }
 
-void syserror(const char *s)
-{
+void syserror(const char *s){
 	extern int errno;
-
 	fprintf( stderr, "%s\n", s );
 	fprintf( stderr, " (%s)\n", strerror(errno) );
 	exit( 1 );
 }
-
--- makefile
+++ makefile
@@ -1,26 +1,19 @@
 # Makefile for pipe test
 
-all: pipes.x pipe_demo.x
+all: pipes.x
 
 # $@ is make shorthand for the thing on the left side of the colon
 #   (pipes.x in this case)
 # $^ stands for everything on the right side of the colon (the .o files)
 pipes.x: main.o parsetools.o
-	gcc -o $@ $^
-
-pipe_demo.x: pipe_demo.o
-	gcc -o $@ $^
+	gcc -g -o $@ $^
 
 # $< is the first item after the colon (main.c here)
 main.o: main.c parsetools.h constants.h
-	gcc -c -o $@ $<
+	gcc -g -c -o $@ $<
 
 parsetools.o: parsetools.c constants.h
-	gcc -c -o $@ $<
-
-pipe_demo.o: pipe_demo.c
-	gcc -c -o $@ $<
+	gcc -g -c -o $@ $<
 
 clean:
 	rm -f *.x *.o *~
-
